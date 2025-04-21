11234eddf0e1306f6d583a40aadb34bcc8d26e87
Gaurab-Moharana <gauravmoharana8560@gmail.com>
2025-04-21 20:22:03 +0530

    file changed

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -23,37 +23,37 @@ const Dashboard = () => {
   return (
     <div className="flex h-screen  bg-gradient-to-r from-black to-blue-950">
       {/* Sidebar */}
-      <div className="w-64 bg-gray-800 p-5">
-        <div className="flex items-center flex-col mb-10">
-          <img src={logo} alt="Profile" className="rounded-full h-20 w-20" />
-          <h2 className="text-lg font-bold text-white mt-4">Admin</h2>
-        </div>
-        <nav className="flex flex-col space-y-4">
-          <Link to={"/admin/our-courses"}>
-            <button className="w-full bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded">
-              Our Courses
-            </button>
-          </Link>
-          <Link to={"/admin/create-course"}>
-            <button className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded ">
-              Create Course
-            </button>
-          </Link>
-          <Link to={"/"}>
-            <button className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded ">
-              Home
-            </button>
-          </Link>
-          <Link to={"/admin/login"}>
-            <button
-              onClick={handleAdminLogout}
-              className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded"
-            >
-              Logout
-            </button>
-          </Link>
-        </nav>
+
+      <div className="flex items-center flex-col mb-10">
+        <img src={logo} alt="Profile" className="rounded-full h-20 w-20" />
+        <h2 className="text-lg font-bold text-white mt-4">Admin</h2>
       </div>
+      <nav className="flex flex-col space-y-4">
+        <Link to={"/admin/our-courses"}>
+          <button className="w-full bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded">
+            Our Courses
+          </button>
+        </Link>
+        <Link to={"/admin/create-course"}>
+          <button className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded ">
+            Create Course
+          </button>
+        </Link>
+        <Link to={"/"}>
+          <button className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded ">
+            Home
+          </button>
+        </Link>
+        <Link to={"/admin/login"}>
+          <button
+            onClick={handleAdminLogout}
+            className="w-full  bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded"
+          >
+            Logout
+          </button>
+        </Link>
+      </nav>
+
       <div className="flex-1 flex items-center justify-center text-white font-bold ml-5 mt-10 text-2xl ">
         Welcome Admin!!!
       </div>
