92eec838821638031f85f351891afb9d00785b74
Gaurab-Moharana <gauravmoharana8560@gmail.com>
2025-04-21 20:35:16 +0530

    file changed

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -25,10 +25,10 @@ const Dashboard = () => {
       {/* Sidebar */}
 
       <div className="flex items-center flex-col mb-10">
-        <img src={logo} alt="Profile" className="rounded-full h-20 w-20" />
+        <img src={logo} alt="Profile" className="rounded-full mt-2 h-20 w-20" />
         <h2 className="text-lg font-bold text-white mt-4">Dashboard</h2>
       </div>
-      <nav className="flex flex-col  w-80">
+      <nav className="flex items-center flex-col space-y-4 w-80">
         <Link to={"/admin/our-courses"}>
           <button className="w-full bg-green-700 hover:bg-green-600 text-white py-2 cursor-pointer rounded">
             Our Courses
