0cbb1ff06f3c1c440b9e6b05ea9261dcc0b7267e
Gaurab-Moharana <gauravmoharana8560@gmail.com>
2025-04-21 20:14:10 +0530

    file changed

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -54,7 +54,7 @@ const Dashboard = () => {
           </Link>
         </nav>
       </div>
-      <div className="flex-1 flex items-center justify-center text-white font-bold text-2xl ">
+      <div className="flex-1 flex items-center justify-center text-white font-bold ml-5 text-2xl ">
         Welcome Admin!!!
       </div>
     </div>
